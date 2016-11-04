1c76c870b194dca781ecd88ba2525c3b8329677c
whitequark <whitequark@whitequark.org>
2016-11-04 08:28:09 +0000

    Make EIA footprints of specific size constants, not classes.

--- pcbhdl/library/package/passive.py
+++ pcbhdl/library/package/passive.py
@@ -33,22 +33,11 @@ class EIATwoTerminalSMT(Footprint):
         super().__init__(name, pads)
 
 
-class EIA_I_0402(EIATwoTerminalSMT):
-    """EIA imperial 0402 SMT passive package."""
+EIA_I_0402 = EIATwoTerminalSMT("EIA_I_0402", pad_width=0.5, pad_height=0.5, package_width=1.5)
+"""EIA imperial 0402 SMT passive package."""
 
-    def __init__(self):
-        super().__init__("EIA_I_0402", pad_width=0.5, pad_height=0.5, package_width=1.5)
+EIA_I_0603 = EIATwoTerminalSMT("EIA_I_0603", pad_width=0.8, pad_height=1.0, package_width=2.6)
+"""EIA imperial 0603 SMT passive package."""
 
-
-class EIA_I_0603(EIATwoTerminalSMT):
-    """EIA imperial 0603 SMT passive package."""
-
-    def __init__(self):
-        super().__init__("EIA_I_0603", pad_width=0.8, pad_height=1.0, package_width=2.6)
-
-
-class EIA_I_0805(EIATwoTerminalSMT):
-    """EIA imperial 0805 SMT passive package."""
-
-    def __init__(self):
-        super().__init__("EIA_I_0805", pad_width=1.2, pad_height=1.3, package_width=3.4)
+EIA_I_0805 = EIATwoTerminalSMT("EIA_I_0805", pad_width=1.2, pad_height=1.3, package_width=3.4)
+"""EIA imperial 0805 SMT passive package."""
--- pcbhdl/test/library/test_package.py
+++ pcbhdl/test/library/test_package.py
@@ -4,7 +4,7 @@ from pcbhdl.library.package.passive import *
 
 class TestPackage(unittest.TestCase):
     def test_eia_two_terminal(self):
-        pkg = EIA_I_0603()
+        pkg = EIA_I_0603
         self.assertEqual(pkg.name, "EIA_I_0603")
         self.assertEqual(pkg.pads[0].name,   "1")
         self.assertEqual(pkg.pads[0].center, (-0.9, 0.0))
